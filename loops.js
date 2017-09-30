b0e6e36a570a9a9b2a66d5d4136b79bfe3c8a665
Nikita Neganov <nikitaneganov@icloud.com>
2017-09-30 12:38:52 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -1,3 +1,6 @@
-for (let i = 0; i < 25; i++){
-  console.log(`I am ${i} strange loop.`)
+function forLoop(array){
+  
+  for (let i = 0; i < 25; i++){
+    array.push(`I am ${i} strange loop.`)
+  }
 }
